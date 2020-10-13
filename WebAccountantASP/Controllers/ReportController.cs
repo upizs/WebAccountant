103b07f61f7cf51388dfed8066cfae6e935c27ea
Toms Upis <toms.upizs@gmail.com>
2020-10-13 22:32:32 +0100

    Added Report Action and view. At the moment just records transactions.

--- WebAccountantASP/Controllers/ReportController.cs
+++ WebAccountantASP/Controllers/ReportController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using WebAccountantASP.Models;
+using WebAccountantASP.ViewModel;
 
 namespace WebAccountantASP.Controllers
 {
@@ -30,9 +31,30 @@ namespace WebAccountantASP.Controllers
         // GET: Report
         public ActionResult Index()
         {
+            
+            DateTime LastThurday = FindLastThursday();
+            var thisWeeksTransactions = _context.Transactions.Where(x => x.Date > LastThurday).ToList() ;
 
+            var reportViewModel = new ReportViewModel()
+            {
+                Transaction = new Transaction(),
+                Transactions = thisWeeksTransactions
 
-            return View();
+            };
+
+            return View(reportViewModel);
+        }
+
+        public DateTime FindLastThursday()
+        {
+            
+            var date = DateTime.Now;
+            while (date.DayOfWeek != DayOfWeek.Thursday)
+            {
+                date = date.AddDays(-1);
+            }
+
+            return date;
         }
     }
 }
---
+++ WebAccountantASP/ViewModel/ReportViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using WebAccountantASP.Models;
+
+namespace WebAccountantASP.ViewModel
+{
+    public class ReportViewModel
+    {
+        public Transaction Transaction { get; set; }
+        public IEnumerable<Transaction> Transactions { get; set; }
+    }
+}
